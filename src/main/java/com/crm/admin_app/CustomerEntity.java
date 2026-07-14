16bc37cc160eb15874cbefa99b57a6c0b31029ed
jornJp0201 <uda.job.hunting@gmail.com>
2026-07-14 12:06:45 +0900

    entityクラスを更新しました

--- src/main/java/com/crm/admin_app/CustomerEntity.java
+++ src/main/java/com/crm/admin_app/CustomerEntity.java
@@ -1,16 +1,47 @@
 package com.crm.admin_app;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 @Entity
 @Table(name="customers")
 public class CustomerEntity {
 
     @Id
-    @Column(name="customer_id")
+    @Column(name="customer_id", nullable=false,unique=true,length=10)
     private int customerId;
-    
+
+    @Column(name="customer_name",nullable=false)
+    private String customerName;
+
+    @Column(name="date",nullable=false,updatable=false)
+    private LocalDate date;
+
+    @Column(name="active")
+    private String active;
+
+    public CustomerEntity(){
+    }
+
+    public CustomerEntity(int customerId,String customerName,LocalDate date,String active){
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.date = date;
+        this.active = active;
+    }
+
+    public void setCustomerId(int customerId){
+        this.customerId=customerId;
+    }
+
+    public int getCustomerId(){
+        return customerId;
+    }
+
+    public void setCustomerName(String customerName){
+        this.customerName=customerName;
+    }
 }
