a2aa9de49d7461dadd4d55522623095f63127ff9
jornJp0201 <uda.job.hunting@gmail.com>
2026-07-15 09:43:48 +0900

    メソッドの作成を行いました

---
+++ src/main/java/com/crm/admin_app/CostomerRepository.java
@@ -0,0 +1,16 @@
+package com.crm.admin_app;
+
+//import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+
+
+public interface CostomerRepository extends JpaRepository<CustomerEntity, String> {
+
+    
+}
+
+
+    
+
--- src/main/java/com/crm/admin_app/CustomerController.java
+++ src/main/java/com/crm/admin_app/CustomerController.java
@@ -1,6 +1,7 @@
 package com.crm.admin_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.Controller;
@@ -9,10 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customers")
 public class CustomerController {
     
-@GetMapping("/test")
+@GetMapping("/test")//これはテスト用のエンドポイントです。実際のアプリケーションには不要です。
     public void test(){
 
     }
 
 
 }
+
+
+@Service
+class CustomerService{
+    private final CostomerRepository costomerRepository;
+
+    public CustomerService(CostomerRepository costomerRepository){
+        this.costomerRepository = costomerRepository;
+    }
+
+}
+
