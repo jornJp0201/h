48f62d99ba6945b09b9b8da044b9fec8e3032691
jornJp0201 <uda.job.hunting@gmail.com>
2026-07-18 23:37:53 +0900

    バリデーションや整合性の向上を行いました。一部コードを機能を変えないまま見やすいように修正しました。

--- src/main/java/com/crm/admin_app/CustomerEntity.java
+++ src/main/java/com/crm/admin_app/CustomerEntity.java
@@ -2,46 +2,66 @@ package com.crm.admin_app;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;//バリデーション
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 @Entity
 @Table(name="customers")
 public class CustomerEntity {
 
     @Id
-    @Column(name="customer_id", nullable=false,unique=true,length=10)
-    private String customerId;
+    @GeneratedValue(strategy=GenerationType.IDENTITY) //これを使用することでcustomerIdが自動で生成され、採番の衝突を防ぐことができる
+    private Long customerId;
 
     @Column(name="customer_name",nullable=false)
+    @NotBlank(message="顧客名は必須です")
     private String customerName;
 
-    @Column(name="date",nullable=false,updatable=false)
+    @Email(message = "有効なメールアドレスの形式で入力してください")
+    private String customerEmail;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")//日付のフォーマットを指定することで整合性を保っています。
+    @PastOrPresent(message="日付は過去現在にする必要があります")
+    @NotNull(message="日付は必須です")
     private LocalDate date;
 
-    @Column(name="active")
-    private String active;
+    @Column(name="text",nullable=true)
+    private String text;
+
+    @Column(name="active")//非表示表示用
+    private Boolean active = true;
 
 
     //コンストラクタ
     public CustomerEntity(){
     }
 
-    public CustomerEntity(String customerId,String customerName,LocalDate date,String active){
+    public CustomerEntity(Long customerId,String customerName,String customerEmail,LocalDate date,String text,Boolean active){
         this.customerId = customerId;
         this.customerName = customerName;
+        this.customerEmail = customerEmail;
         this.date = date;
+        this.text = text;
         this.active = active;
     }
 
     //ゲッターとセッター
 
-    public void setCustomerId(String customerId){
+    public void setCustomerId(Long customerId){
         this.customerId=customerId;
     }
 
-    public String getCustomerId(){
+    public Long getCustomerId(){
         return customerId;
     }
 
@@ -53,6 +73,14 @@ public class CustomerEntity {
         return customerName;
     }
 
+    public void setCustomerEmail(String customerEmail){
+        this.customerEmail=customerEmail;
+    }
+     
+    public String getCustomerEmail(){
+        return customerEmail;
+    }
+
     public void setDate(LocalDate date){
         this.date=date;
     }
@@ -61,11 +89,19 @@ public class CustomerEntity {
         return date;
     }
 
-    public void setActive(String active){
+    public void setText(String text){
+        this.text=text;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public void setActive(Boolean active){
         this.active=active;
     }
 
-    public String getActive(){
+    public Boolean getActive(){
         return active;
     }
 }
