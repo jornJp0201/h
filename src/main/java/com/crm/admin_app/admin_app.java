be411e39c3f3461fe8f030d9c3dc2a5a720ec948
jornJp0201 <uda.job.hunting@gmail.com>
2026-07-21 13:24:37 +0900

    直接/にアクセスするのではなくリダイレクトを使用し間接的にアクセスする方法に切り替えました

--- src/main/java/com/crm/admin_app/admin_app.java
+++ src/main/java/com/crm/admin_app/admin_app.java
@@ -1,12 +1,20 @@
 package com.crm.admin_app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class admin_app {
 
-    @GetMapping("/")
+    @GetMapping("/index")
      public String index(){
         return "index";
+     }
+
+     @GetMapping("/")
+     public String first_index(){
+        return "redirect:/customers/table";
      }
 
      @GetMapping("/create")
